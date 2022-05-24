291dbe061b1b86a7fcd30c82770aa6540e810fc7
NicteAC <carola.aliaga@gmail.com>
2022-05-23 22:03:32 -0400

    Fin

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+//Tooltips aplicado en la seccion Quienes Somos
+let tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+  $(function () {
+    //Esta es la función general de jquery
+    //Todos los métodos de jquery van acá adentro
+  
+    //Oculta informacion al hacer click en el titulo de las tarjetas
+    $(".card-title").click(function () {
+      $(".card-text2").toggle();
+    });
+    $(document).ready(function () {
+        $(window).scroll(function () {
+            let scroll = $(window).scrollTop();
+            if (scroll > 800) {
+                $(".navbar").css("background", "#000");
+            }
+
+            else {
+                $(".navbar").css("background", "none");
+            }
+        })
+    })
+   
+  });
+  
